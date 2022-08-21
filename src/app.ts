c16eec66b973501aba04c0ef9926d35765f57a20
Khim Bahadur Gurung <khimbahadurgurung@khims-mbp.speedport.ip>
2022-08-21 21:41:16 +0200

    cors origin changed

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ const app: Application = express();
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(cors({
-    origin: "http://localhost:3000"
+    origin: "*"
 }));
 
 app.use('/users', userRoute);
