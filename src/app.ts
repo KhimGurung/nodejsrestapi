d23f27806dd62a3d4afb12fb1f5920857bc4a93f
Khim Bahadur Gurung <khimbahadurgurung@Khims-MacBook-Pro.local>
2022-08-22 13:40:42 +0200

    cors origin error

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import express, { Application } from 'express';
 import cors from 'cors';
-import auth from './middleware/auth';
+// import https from 'https';
+// import fs from 'fs';
+// import path from 'path';
+// import auth from './middleware/auth';
 import connectDB from './database/db';
 import errorHandler from './middleware/error_handler';
 import userRoute from './routes/user_routes';
@@ -9,9 +12,15 @@ import { getRefreshToken } from './controllers/refresh_token_controller';
 
 const app: Application = express();
 
-app.use(cors({
-    'origin': 'https://reactjs-metamask.herokuapp.com/'
-}));
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "https://reactjs-metamask.herokuapp.com/");
+//     res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST, OPTIONS');
+//     next();
+//  });
+// app.use(cors({
+//     'origin': 'https://reactjs-metamask.herokuapp.com/'
+// }));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
 
@@ -25,3 +34,13 @@ app.listen(port, () => {
     console.log(`server is listening on port: ${port}`);
     connectDB();
 });
+
+// const sslServer = https.createServer({
+//     key: fs.readFileSync(path.join(path.dirname(path.basename(__dirname)), 'cert', 'key.pem')),
+//     cert: fs.readFileSync(path.join(path.dirname(path.basename(__dirname)), 'cert', 'cert.pem'))
+// }, app)
+// sslServer.listen(port, () => {
+//     console.log(`server is listening on port: ${port}`);
+//     connectDB();
+// })
+
